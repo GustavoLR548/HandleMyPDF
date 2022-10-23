9790e98c62e398cc264b63c70c55887a45b8dfa1
Homenique V <homenique.t@gmail.com>
2022-10-23 13:14:19 -0300

    Now SavePDF is a separate function, which will allow you to deal with saving a file

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ import sys
 import numpy as np
 from os.path import exists
 
-def extract_range_of_pages(pages_range : str, pdf_name : str) -> None: 
-    
+
+def salvePDF(path: str, output_file: PdfFileWriter) -> None:
+    # Remove o pdf do fim da string e adiciona um _out
+    path = path[:-4] + "_Out.pdf"
+
+    with open(path, 'wb') as f:
+        output_file.write(f)
+
+
+def extract_range_of_pages(pages_range: str, pdf_name: str) -> PdfFileWriter:
+
     extract_page = []
-    outputFile = PdfFileWriter()
+    output_file = PdfFileWriter()
     pages_to_include = set(map(int, pages_range.split(',')))
-    file = PdfFileReader(pdf_name,'rb')
+    file = PdfFileReader(pdf_name, 'rb')
 
     print(pages_to_include)
 
@@ -17,56 +26,55 @@ def extract_range_of_pages(pages_range : str, pdf_name : str) -> None:
 
         if (page+1) in pages_range:
             extract_page = file.getPage(page)
-            outputFile.addPage(extract_page)
-        
-    with open('out.pdf', 'wb') as f:
-        outputFile.write(f)
+            output_file.addPage(extract_page)
 
-def delete_pages(pdf_name : str, pages : str) -> None:
+
+def delete_pages(pdf_name: str, pages: str) -> PdfFileWriter:
 
     pages_to_delete = list(map(int, pages.split(',')))
-    infile = PdfFileReader(pdf_name, 'rb')
-    output = PdfFileWriter()
+    file = PdfFileReader(pdf_name, 'rb')
+    output_file = PdfFileWriter()
 
-    for i in range(infile.getNumPages()):
+    for i in range(file.getNumPages()):
         if (i+1) not in pages_to_delete:
-            p = infile.getPage(i)
-            output.addPage(p)
+            p = file.getPage(i)
+            output_file.addPage(p)
+   ## print("Output File: \n"+ output_file)
 
-
-    with open('newfile.pdf', 'wb') as f:
-        output.write(f)
-
-
+    return output_file
 
 
 def helper():
     print('Usage:main.py [command] <path>')
     print('-d --delete-pages <page>                      Delete pages.')
-    print('-e --extract-range-of-pages [<page>,<page>]   Extract page by a range.')
+    print(
+        '-e --extract-range-of-pages [<page>,<page>]   Extract page by a range.')
 
     print('Options:\n')
     print('-h --help     Show this screen.')
     print('--version     Show version.')
-    
-  
-  
+
+
+def validation_file() -> bool:
+    return True
+
 
 if __name__ == '__main__':
 
     argv_len = len(sys.argv)
-
 
     if argv_len == 1:
         print("[ERROR]: Not enough arguments")
         sys.exit(1)
 
     command = sys.argv[1]
-    
+
     if command == '--help' or command == '--h':
         helper()
 
     if exists(sys.argv[-1]):
+
+        output_file = PdfFileWriter()
 
         if command == '--delete-pages' or command == '--d':
 
@@ -74,14 +82,17 @@ if __name__ == '__main__':
                 print("[ERROR]: Not enough arguments")
                 sys.exit(1)
 
-            delete_pages(sys.argv[-1], sys.argv[2])
+            output_file.appendPagesFromReader(delete_pages(sys.argv[-1], sys.argv[2]))
 
         if command == '--extract-range-of-pages' or command == '--e':
             print('extract-range-of-pages')
             if argv_len != 4:
                 print("[ERROR]: Not enough arguments")
                 sys.exit(1)
+                
+            output_file.appendPagesFromReader(extract_range_of_pages(sys.argv[-1], sys.argv[2]))
 
-            extract_range_of_pages(sys.argv[-1],sys.argv[2])
+        salvePDF(sys.argv[-1],output_file)
+
     else:
         print(f"\n[ERROR]: The file {sys.argv[-1]} does not exist\n")
