d4ab319bf8e64de09edb313de083c9582d732098
Lucas Santiago <lu.santi.oli@gmail.com>
2022-10-30 20:46:19 -0300

    refact: big refactoring in code

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
-from pydoc import Helper
 from PyPDF2 import PdfFileWriter, PdfFileReader
-import sys
-import numpy as np
-from os.path import exists
+import sys, os
 
-
-def salvePDF(path: str, output_file: PdfFileWriter) -> None:
-    # Remove o pdf do fim da string e adiciona um _out
-    path = path[:-4] + "_Out.pdf"
+def savePdf(path: str, output_file: PdfFileWriter) -> None:
+    # Get the pdf's name and sum up with _out.pdf
+    path = path.split('.pdf')[0] + "_out.pdf"
 
     with open(path, 'wb') as f:
         output_file.write(f)
 
 
-def extract_range_of_pages(pages_range: str, pdf_name: str) -> PdfFileWriter:
-
-    extract_page = []
+def extractRangeOfPages(pages_range: str, pdf_name: str) -> PdfFileWriter:
+    
     output_file = PdfFileWriter()
-    pages_to_include = set(map(int, pages_range.split(',')))
+    pages_to_include = map(int, pages_range.split(','))
+    excluding_duplicated_pages = set(pages_to_include)
+    pages = excluding_duplicated_pages
     file = PdfFileReader(pdf_name, 'rb')
 
-    print(pages_to_include)
+    print(pages)
 
     for page in range(file.getNumPages()):
 
-        if (page + 1) in pages_to_include:
+        if (page + 1) in pages:
             extract_page = file.getPage(page)
             output_file.addPage(extract_page)
 
     return output_file
 
 
-def delete_pages(pdf_name: str, pages: str) -> PdfFileWriter:
+def deletePages(pdf_name: str, pages: str) -> PdfFileWriter:
 
     pages_to_delete = list(map(int, pages.split(',')))
     file = PdfFileReader(pdf_name, 'rb')
@@ -46,56 +43,41 @@ def delete_pages(pdf_name: str, pages: str) -> PdfFileWriter:
 
 
 def helper():
-    print('Usage:main.py [command] <path>')
+    print('Usage: main.py [command] <path>')
     print('-d --delete-pages <page>                      Delete pages.')
-    print(
-        '-e --extract-range-of-pages [<page>,<page>]   Extract page by a range.')
+    print('-e --extract-range-of-pages [<page>,<page>]   Extract page by a range.')
 
     print('Options:\n')
     print('-h --help     Show this screen.')
     print('--version     Show version.')
 
-# Função para validar arquivo
-def validation_file(path: str) -> bool:
-    return exists(path)
 
-
-if __name__ == '__main__':
-
-    argv_len = len(sys.argv)
-
-    if argv_len == 1:
-        print("[ERROR]: Not enough arguments")
-        sys.exit(1)
-
-    command = sys.argv[1]
-
-    if command == '--help' or command == '--h':
+def main():    
+    if len(sys.argv) <= 1 or sys.argv[1] == '--help' or sys.argv[1] == '-h':
         helper()
-
-    if validation_file(sys.argv[-1]):
-
-        output_file = PdfFileWriter()
-
-        if command == '--delete-pages' or command == '--d':
-
-            if argv_len != 4:
-                print("[ERROR]: Not enough arguments")
-                sys.exit(1)
-
-            output_file.appendPagesFromReader(
-                delete_pages(sys.argv[-1], sys.argv[2]))
-
-        if command == '--extract-range-of-pages' or command == '--e':
     
-            if argv_len != 4:
-                print("[ERROR]: Not enough arguments")
-                sys.exit(1)
+    elif os.path.exists(sys.argv[-1]):
+        output_file = PdfFileWriter()
+        argv_len = len(sys.argv)
 
-            output_file.appendPagesFromReader(
-                extract_range_of_pages(sys.argv[2], sys.argv[-1]))
+        command = sys.argv[1]
+        if argv_len != 4:
+            print("[ERROR]: Not enough arguments")
+        
+        else:
+            if command == '--delete-pages' or command == '-d':
+                output_file.appendPagesFromReader(
+                    deletePages(sys.argv[-1], sys.argv[2]))
 
-        salvePDF(sys.argv[-1], output_file)
+            elif command == '--extract-range-of-pages' or command == '-e':
+                output_file.appendPagesFromReader(
+                    extractRangeOfPages(sys.argv[2], sys.argv[-1]))
+
+            savePdf(sys.argv[-1], output_file)
 
     else:
         print(f"\n[ERROR]: The file {sys.argv[-1]} does not exist\n")
+    
+
+if __name__ == '__main__':
+    main()
